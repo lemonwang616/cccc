71287fa2f58adfdba817b3ab320ef5d343d3e329
lemonwang616 <lemon.wang.poppy@gmail.com>
2018-10-16 16:07:15 +0800

    leetcode 188

--- main.cpp
+++ main.cpp
@@ -18,53 +18,55 @@ int max(int a,int b)
 	return (a>b?a:b);
 }
 
-    int maxProfit(vector<int> prices) { 
-    	//计算相邻两个差值和  
-		int n=prices.size();
-		if(n==0)
+    int maxProfit(int k,vector<int> prices) { 
+		int profit=0;
+		int size=prices.size();
+		if(size<2)
 		{
 			return 0;
 		}
-		vector<int> left(n,0);//left[0]为0 
-		vector<int> reft(n,0);//right[n-1]为0 
-		int i;
-		int profit=0;
-		int buyin=prices[0];
-		for(i=1;i<n;i++)
+		int i,j;
+		//当买卖次数>股票数时  特殊讨论 
+		if(k>=size)
 		{
-			if(prices[i]-buyin>profit)
+			int profit=0;
+			for(i=1;i<size;i++)
 			{
-				profit=prices[i]-buyin;
+				if(prices[i]>prices[i-1])
+				{
+					profit=profit+prices[i]-prices[i-1];
+				}
 			}
-			if(buyin>prices[i])
-			{
-				buyin=prices[i];
-			}
-			left[i]=profit;//从 0到i的最大值 
+			return profit;
 		}
-		profit=0;
-		int buyout=prices[n-1];
-		for(i=n-2;i>=0;i--)
+		vector<int> global[2];
+		vector<int> local[2];
+		for(i=0;i<2;i++)
 		{
-			if(buyout-prices[i]>profit)
-			{
-				profit=buyout-prices[i];
-			}
-			if(prices[i]>buyout)
-			{
-				buyout=prices[i];
-			}
-			reft[i]=profit;//求price[n-1]到prices[i]的最大值 
+			local[i].assign(k+1,0);
+			global[i].assign(k+1,0);
 		}
-		profit=0;
-		for(i=0;i<n;i++)
+
+		int flag=0;
+
+		for(i=1;i<size;i++)
 		{
-			if(left[i]+reft[i]>profit)
+			
+				flag=(flag+1)%2;
+			for(j=1;j<=k;j++)
 			{
-				profit=left[i]+reft[i];
+				int diff=prices[i]-prices[i-1];
+			
+				local[flag][j]=max(global[1-flag][j-1]+max(diff,0),local[1-flag][j]+diff);
+				global[flag][j]=max(local[flag][j],global[1-flag][j]);	
+			//	cout<<"localarr"<<i<<j<<": "<<local[flag][j]<<endl;
+			//	cout<<"globalar: "<<i<<j<<": "<<global[flag][j]<<endl;
 			}
 		}
-		return profit;
+		
+
+		
+		return global[flag][k];
     }
 
 int main(int argc, char** argv) {
@@ -77,8 +79,9 @@ int main(int argc, char** argv) {
 	cin>>temp;
 	prices.push_back(temp);
 	}
-	
-	cout<<maxProfit(prices)<<endl;
+	int k;
+	cin>>k;
+	cout<<maxProfit(k,prices)<<endl;
 	return 0;
 	
 }
